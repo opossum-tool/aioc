457c8a3eea6bc591c58ace95e28b689f53fb596c
Maximilian Huber <maximilian.huber@tngtech.com>
2021-09-17 19:13:46 +0200

    update submodules before build
    Signed-off-by: Maximilian Huber <maximilian.huber@tngtech.com>

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -5,5 +5,8 @@ cd "$(dirname "$0")"
 
 export DOCKER_BUILDKIT=1
 
+git submodule init
+git submodule update --remote
+
 docker build oss-review-toolkit --tag ort/with_opossum --network=host
 docker build . --tag opossum/aioc --network=host
